43f6db0fac42e240780dcd66fce376cb4494178e
Fabi-Chan <73954978+FabiChan99@users.noreply.github.com>
2023-08-15 13:56:09 +0200

    Added Exception handling, when a improper token is provided

--- KawaiiClient.cs
+++ KawaiiClient.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Net.Http;
 using System.Net.Http.Headers;
+using System.Security.Authentication;
 using System.Text.Json;
 using System.Threading.Tasks;
 using KawaiiAPI.NET.Enums;
@@ -31,12 +32,21 @@ namespace KawaiiAPI.NET
         /// <summary>
         /// Returns a random GIF URL from the specified endpoint.
         /// </summary>
+        /// <exception cref="AuthenticationException">Throws when a invalid Auth Token is provided.</exception>
         public async Task<string> GetRandomGifAsync(KawaiiGifType type)
         {
             var response = await _httpClient.GetAsync($"{type}?token={_token}");
             response.EnsureSuccessStatusCode();
             string jsonResponse = await response.Content.ReadAsStringAsync();
             using JsonDocument document = JsonDocument.Parse(jsonResponse);
+            if (document.RootElement.TryGetProperty("error", out JsonElement errorResponse))
+            {
+                if (errorResponse.GetString().Contains("Invalid authentication token!"))
+                {
+                    throw new AuthenticationException(errorResponse.GetString());
+                }
+
+            }
             if (document.RootElement.TryGetProperty("response", out JsonElement responseElement))
             {
                 string? gifUrl = responseElement.GetString();
